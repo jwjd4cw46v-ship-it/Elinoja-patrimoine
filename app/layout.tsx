e19c4a9d8d548c518f9191e296a188452e2898c0
jwjd4cw46v-ship-it <jwjd4cw46v@privaterelay.appleid.com>
2026-07-05 23:47:50 +0200

    Fix apple-touch-icon href path

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Elinoja" />
         {/* Icons Apple */}
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
       </head>
       <body className={`${inter.variable} antialiased`}>
         {children}
