bd9469e785db24c3191287fa2cf5a64b8699e38c
jwjd4cw46v-ship-it <jwjd4cw46v@privaterelay.appleid.com>
2026-07-05 22:28:54 +0200

    Update AdminForumPage content and structure

--- app/admin/forum/page.tsx
+++ app/admin/forum/page.tsx
@@ -1,10 +1,9 @@
-⁠'use client'
+'use client'
 
 export default function AdminForumPage() {
   return (
-    <div className="p-10">
-      <h1 className="text-white">Page Forum Admin en cours de test</h1>
+    <div>
+      <h1>Page Test</h1>
     </div>
   )
 }
-
