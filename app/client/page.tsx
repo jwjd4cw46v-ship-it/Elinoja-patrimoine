7dcb2373501715e8951af2a4af587ad88857f356
jwjd4cw46v-ship-it <jwjd4cw46v@privaterelay.appleid.com>
2026-07-26 17:59:00 +0200

    Adjust background size from 75% to 65%

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -203,7 +203,7 @@ export default function ClientDashboard() {
               className="absolute inset-0"
               style={{
                 backgroundImage: `url('${s.image}')`,
-                backgroundSize: '75%',
+                backgroundSize: '65%',
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat',
                 filter: 'brightness(1.25) saturate(1.4)',
